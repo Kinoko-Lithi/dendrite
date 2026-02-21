0d23268e629e8a4c45e2871df4b0b571171059b9
Kinoko <elenimixanthopoulou@gmail.com>
2026-02-21 21:03:04 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,24 +2,37 @@ document.addEventListener("DOMContentLoaded", function(){
 
 const videos = document.querySelectorAll(".click-video");
 
+
 const overlay = document.createElement("div");
 
 overlay.className = "video-overlay";
 
+
 overlay.innerHTML = `
+
 <div class="video-overlay-content">
+
 <iframe allow="autoplay; fullscreen" allowfullscreen></iframe>
+
 </div>
+
 `;
 
+
 document.body.appendChild(overlay);
+
 
 const iframe = overlay.querySelector("iframe");
 
 
+
+/* OPEN */
+
 videos.forEach(video => {
 
-video.addEventListener("click", function(){
+video.addEventListener("click", function(e){
+
+e.stopPropagation();
 
 iframe.src = this.dataset.src;
 
@@ -30,12 +43,44 @@ overlay.classList.add("active");
 });
 
 
+
+/* CLOSE CLICK */
+
 overlay.addEventListener("click", function(){
+
+closeVideo();
+
+});
+
+
+
+/* CLOSE ESC */
+
+document.addEventListener("keydown", function(e){
+
+if(e.key === "Escape"){
+
+closeVideo();
+
+}
+
+});
+
+
+
+/* CLOSE FUNCTION */
+
+function closeVideo(){
 
 overlay.classList.remove("active");
 
+setTimeout(() => {
+
 iframe.src = "";
 
-});
+}, 400);
+
+}
+
 
 });
