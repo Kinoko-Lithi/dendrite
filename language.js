454d9598d280f82b224a156241357360257f2db1
Kinoko <elenimixanthopoulou@gmail.com>
2026-03-06 21:26:00 +0200

    Create language.js

---
+++ language.js
@@ -0,0 +1,22 @@
+// translations.js
+
+const translations = {
+  en: {
+    home: "Home",
+    works: "Works",
+    videos: "Videos",
+    images: "Images",
+    about: "About",
+    contact: "Contact",
+    copyright: "© 2026 Dendrite Creative Studio"
+  },
+  el: {
+    home: "Αρχική",
+    works: "Έργα",
+    videos: "Βίντεο",
+    images: "Εικόνες",
+    about: "Σχετικά",
+    contact: "Επικοινωνία",
+    copyright: "© 2026 Δημιουργικό Στούντιο Dendrite"
+  }
+};
